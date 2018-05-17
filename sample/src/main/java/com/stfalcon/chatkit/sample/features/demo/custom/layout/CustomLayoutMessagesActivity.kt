2fde9eb435eb4d27de3d920d3fdda668c5142580
thanhbv <thanhbv@sandinh.net>
2018-05-17 14:02:59 +0700

    migrate to kotlin - cont

---
+++ sample/src/main/java/com/stfalcon/chatkit/sample/features/demo/custom/layout/CustomLayoutMessagesActivity.kt
@@ -0,0 +1,63 @@
+package com.stfalcon.chatkit.sample.features.demo.custom.layout
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+
+import com.stfalcon.chatkit.messages.MessageHolders
+import com.stfalcon.chatkit.messages.MessageInput
+import com.stfalcon.chatkit.messages.MessagesListAdapter
+import com.stfalcon.chatkit.sample.R
+import com.stfalcon.chatkit.sample.common.data.fixtures.*
+import com.stfalcon.chatkit.sample.common.data.model.Message
+import com.stfalcon.chatkit.sample.features.demo.DemoMessagesActivity
+import com.stfalcon.chatkit.sample.utils.AppUtils
+import kotlinx.android.synthetic.main.activity_custom_layout_messages.*
+
+class CustomLayoutMessagesActivity : DemoMessagesActivity(),
+        MessagesListAdapter.OnMessageLongClickListener<Message>,
+        MessageInput.InputListener, MessageInput.AttachmentsListener {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_custom_layout_messages)
+        initAdapter()
+
+        input.setInputListener(this)
+        input.setAttachmentsListener(this)
+    }
+
+    override fun onSubmit(input: CharSequence?): Boolean {
+        messagesAdapter.addToStart(
+                getTextMessage(input!!.toString()), true)
+        return true
+    }
+
+    override fun onAddAttachments() {
+        messagesAdapter.addToStart(imageMessage, true)
+    }
+
+    override fun onMessageLongClick(message: Message) {
+        AppUtils.showToast(this, R.string.on_log_click_message, false)
+    }
+
+    private fun initAdapter() {
+        val holdersConfig = MessageHolders()
+                .setIncomingTextLayout(R.layout.item_custom_incoming_text_message)
+                .setOutcomingTextLayout(R.layout.item_custom_outcoming_text_message)
+                .setIncomingImageLayout(R.layout.item_custom_incoming_image_message)
+                .setOutcomingImageLayout(R.layout.item_custom_outcoming_image_message)
+
+        super.messagesAdapter = MessagesListAdapter(super.senderId, holdersConfig, super.imageLoader)
+        super.messagesAdapter.setOnMessageLongClickListener(this)
+        super.messagesAdapter.setLoadMoreListener(this)
+        messagesList.setAdapter(super.messagesAdapter)
+    }
+
+    companion object {
+
+        fun open(context: Context) {
+            context.startActivity(Intent(context, CustomLayoutMessagesActivity::class.java))
+        }
+    }
+}
---
+++ sample/src/main/java/com/stfalcon/chatkit/sample/features/demo/custom/media/CustomMediaMessagesActivity.kt
@@ -0,0 +1,89 @@
+package com.stfalcon.chatkit.sample.features.demo.custom.media
+
+import android.app.AlertDialog
+import android.content.Context
+import android.content.DialogInterface
+import android.content.Intent
+import android.os.Bundle
+
+import com.stfalcon.chatkit.messages.MessageHolders
+import com.stfalcon.chatkit.messages.MessageInput
+import com.stfalcon.chatkit.messages.MessagesListAdapter
+import com.stfalcon.chatkit.sample.R
+import com.stfalcon.chatkit.sample.common.data.fixtures.*
+import com.stfalcon.chatkit.sample.common.data.model.Message
+import com.stfalcon.chatkit.sample.features.demo.DemoMessagesActivity
+import com.stfalcon.chatkit.sample.features.demo.custom.media.holders.IncomingVoiceMessageViewHolder
+import com.stfalcon.chatkit.sample.features.demo.custom.media.holders.OutcomingVoiceMessageViewHolder
+import kotlinx.android.synthetic.main.activity_custom_media_messages.*
+
+class CustomMediaMessagesActivity : DemoMessagesActivity(),
+        MessageInput.InputListener,
+        MessageInput.AttachmentsListener,
+        MessageHolders.ContentChecker<Message>,
+        DialogInterface.OnClickListener {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_custom_media_messages)
+
+        initAdapter()
+
+        input.setInputListener(this)
+        input.setAttachmentsListener(this)
+    }
+
+    override fun onSubmit(input: CharSequence?): Boolean {
+        super.messagesAdapter.addToStart(
+                getTextMessage(input!!.toString()), true)
+        return true
+    }
+
+    override fun onAddAttachments() {
+        AlertDialog.Builder(this)
+                .setItems(R.array.view_types_dialog, this)
+                .show()
+    }
+
+    override fun hasContentFor(message: Message, type: Byte): Boolean {
+        when (type) {
+            CONTENT_TYPE_VOICE -> return (message.voice != null
+                    && message.voice!!.url != null
+                    && !message.voice!!.url.isEmpty())
+        }
+        return false
+    }
+
+    override fun onClick(dialogInterface: DialogInterface, i: Int) {
+        when (i) {
+            0 -> messagesAdapter.addToStart(imageMessage, true)
+            1 -> messagesAdapter.addToStart(voiceMessage, true)
+        }
+    }
+
+    private fun initAdapter() {
+        val holders = MessageHolders()
+                .registerContentType(
+                        CONTENT_TYPE_VOICE,
+                        IncomingVoiceMessageViewHolder::class.java,
+                        R.layout.item_custom_incoming_voice_message,
+                        OutcomingVoiceMessageViewHolder::class.java,
+                        R.layout.item_custom_outcoming_voice_message,
+                        this)
+
+
+        super.messagesAdapter = MessagesListAdapter(super.senderId, holders, super.imageLoader)
+        super.messagesAdapter.enableSelectionMode(this)
+        super.messagesAdapter.setLoadMoreListener(this)
+        this.messagesList.setAdapter(super.messagesAdapter)
+    }
+
+    companion object {
+
+        private val CONTENT_TYPE_VOICE: Byte = 1
+
+        fun open(context: Context) {
+            context.startActivity(Intent(context, CustomMediaMessagesActivity::class.java))
+        }
+    }
+}
